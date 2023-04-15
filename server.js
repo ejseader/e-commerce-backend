298aded4abf32278eee6d45d4633bf9058af8452
Eric Seader <45432901+ejseader@users.noreply.github.com>
2023-04-14 22:48:07 -0400

    imported sequelize connection, synced sequelize models to database

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const routes = require('./routes');
+const PORT = process.env.PORT || 3000
 // import sequelize connection
+const db = require('./config/connection');
+const api_routes = require('./routes/api_routes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+app.use('/api', api_routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+db.sync({ force: false }). then(() => {
+  app.listen(PORT, () => console.log('App listening on port %s', PORT));
 });
